54ecbc9e8b834cd7380e7b578758677364e3c2f6
giancarlopernudisegura <pernudig@gmail.com>
2021-03-03 00:59:13 -0700

    add experimenter variables and constructor

--- code/app/src/main/java/com/example/experiment_automata/Experimenter.java
+++ code/app/src/main/java/com/example/experiment_automata/Experimenter.java
@@ -1,4 +1,14 @@
 package com.example.experiment_automata;
 
 public class Experimenter {
+    private int userId;
+    private ContactInformation info;
+//    private SearchController controller;
+//    private Collection<Experiment> ownedExperiments;
+//    private Collection<Experiment> participatingExperiments;
+
+    Experimenter(ContactInformation info) {
+        userId = 1;
+        this.info = info;
+    }
 }

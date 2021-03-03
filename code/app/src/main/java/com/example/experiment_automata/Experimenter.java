27f367504f69340a0609c10be5e76ef2bd0be75c
giancarlopernudisegura <pernudig@gmail.com>
2021-03-03 00:59:28 -0700

    add experimenter documentation

--- code/app/src/main/java/com/example/experiment_automata/Experimenter.java
+++ code/app/src/main/java/com/example/experiment_automata/Experimenter.java
@@ -1,5 +1,8 @@
 package com.example.experiment_automata;
 
+/**
+ * This is the experimenter. Contains the information that represents a user.
+ */
 public class Experimenter {
     private int userId;
     private ContactInformation info;
@@ -7,6 +10,9 @@ public class Experimenter {
 //    private Collection<Experiment> ownedExperiments;
 //    private Collection<Experiment> participatingExperiments;
 
+    /**
+     * Creates the experimenter. Assigns a user id automatically.
+     */
     Experimenter(ContactInformation info) {
         userId = 1;
         this.info = info;

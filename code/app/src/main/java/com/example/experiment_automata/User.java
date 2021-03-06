66bb21f91b98255f8840f4a94d0815350e9a965e
Pegmode <dchu@ualberta.ca>
2021-03-05 17:44:52 -0700

    changed userId from int to UUID

--- code/app/src/main/java/com/example/experiment_automata/User.java
+++ code/app/src/main/java/com/example/experiment_automata/User.java
@@ -1,10 +1,12 @@
 package com.example.experiment_automata;
+import java.util.UUID;
 
 /**
  * This is the user. Contains the information that represents a user.
  */
 public class User {
-    private int userId;
+    private static String DEFAULT_UUID_STRING = "00000000-0000-0000-0000-000000000000";//move this to a constants class later
+    private UUID userId;//changed from int to UUID
     private ContactInformation info;
 //    private SearchController controller;
 //    private Collection<Experiment> ownedExperiments;
@@ -16,7 +18,7 @@ public class User {
      * the ContactInformation object containing the information for the user
      */
     User(ContactInformation info) {
-        userId = 1;
+        userId = UUID.randomUUID();//generates a random UUID
         this.info = info;
     }
 
@@ -24,7 +26,7 @@ public class User {
      * Creates the stub user class
      */
     User() {
-        userId = 1;
+        userId = UUID.fromString(DEFAULT_UUID_STRING);//hard code UUID for stub to defaultUUIDString
         this.info = new ContactInformation("Individual",
                 "example@ualberta.ca", "780-555-1234");
     }

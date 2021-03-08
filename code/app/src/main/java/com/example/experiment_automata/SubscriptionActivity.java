20b52096bcbb73233ad8d4c5e4311f32706c4bd1
musichead42 <jdsteven@ualberta.ca>
2021-03-08 12:53:02 -0700

    Created subscription adapter and activity to be used to view the experiments that a user has subscribed to

---
+++ code/app/src/main/java/com/example/experiment_automata/SubscriptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.experiment_automata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription);
+    }
+}

97d2d093ec902b029c3dc787a9ce4ad580710cbe
Pegmode <dchu@ualberta.ca>
2021-03-11 00:46:16 -0700

    created QRCode class

---
+++ code/app/src/main/java/com/example/experiment_automata/QRCode.java
@@ -0,0 +1,11 @@
+package com.example.experiment_automata;
+
+/**
+ * Class for interacting with QR codes and barcodes.
+ * Main interface for @see <a href="https://github.com/zxing/zxing">zxing</a>
+ */
+public class QRCode {
+    private boolean isReadable;
+
+
+}

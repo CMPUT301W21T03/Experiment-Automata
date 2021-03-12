835348bb683647d11667afa7e423acf701d37fad
giancarlopernudisegura <pernudig@gmail.com>
2021-03-12 01:44:25 -0700

    add ExperimentManager unit tests

---
+++ code/app/src/test/java/com/example/experiment_automata/ExperimentManagerTest.java
@@ -0,0 +1,100 @@
+package com.example.experiment_automata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExperimentManagerTest {
+    ExperimentManager experimentManager;
+    ArrayList<Experiment> experiments;
+    ArrayList<UUID> experimentReferences;
+    UUID userId;
+
+    @Before
+    public void setup() {
+        ExperimentMaker experimentMaker = new ExperimentMaker();
+        experimentManager = new ExperimentManager();
+        experiments = new ArrayList<>();
+        experimentReferences = new ArrayList<>();
+        userId = UUID.randomUUID();
+        try {
+            Experiment e = experimentMaker.makeExperiment(ExperimentType.Binomial,
+                    "Test Experiment First", 0, false, true, userId);
+            UUID id = e.getExperimentId();
+            experimentReferences.add(id);
+            experiments.add(e);
+            experimentManager.add(id, e);
+
+            e = experimentMaker.makeExperiment(ExperimentType.Binomial,
+                    "Test Experiment Second", 0, true, false, UUID.randomUUID());
+            id = e.getExperimentId();
+            experimentReferences.add(e.getExperimentId());
+            experiments.add(e);
+            experimentManager.add(id, e);
+        } catch (IllegalExperimentException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    public void appending() {
+        assertEquals(experiments.get(0),
+                experimentManager.queryExperiments(experimentReferences).get(0));
+        assertEquals(experiments.get(1),
+                experimentManager.queryExperiments(experimentReferences).get(1));
+        assertEquals(2, experimentManager.getSize());
+        Experiment e = experiments.get(0);
+        assertThrows(IllegalArgumentException.class,
+                () -> experimentManager.add(e.getExperimentId(), e));
+    }
+
+    @Test
+    public void deleting() {
+        UUID id = experiments.get(0).getExperimentId();
+        experimentManager.delete(id);
+        assertEquals(1, experimentManager.getSize());
+    }
+
+    @Test
+    public void ownedExperiments() {
+        ArrayList<Experiment> ownedExperiments = experimentManager.getOwnedExperiments(userId);
+        assertEquals(experiments.get(0), ownedExperiments.get(0));
+    }
+
+    @Test
+    public void publishedExperiments() {
+        ArrayList<Experiment> publishedExperiments = experimentManager.queryPublishedExperiments();
+        // None of the experiments have been published
+        assertEquals(0, publishedExperiments.size());
+        // Publish all the experiments
+        for (Experiment e : experiments) {
+            e.setPublished(true);
+        }
+        publishedExperiments = experimentManager.queryPublishedExperiments();
+        assertEquals(2, publishedExperiments.size());
+    }
+
+    @Test
+    public void stringQuery() {
+        String query = "Test Experiment";
+        assertEquals(2, experimentManager.queryExperiments(query).size());
+
+        query = query.toLowerCase();
+        assertEquals(2, experimentManager.queryExperiments(query).size());
+
+        query = "t";
+        assertEquals(2, experimentManager.queryExperiments(query).size());
+
+        query = "second";
+        assertEquals(1, experimentManager.queryExperiments(query).size());
+
+        query = "first";
+        assertEquals(1, experimentManager.queryExperiments(query).size());
+    }
+}

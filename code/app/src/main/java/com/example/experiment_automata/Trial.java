1bc2aae6dbb6c77609aa079d47a699520dc5afca
giancarlopernudisegura <pernudig@gmail.com>
2021-03-12 12:51:40 -0700

    trial abstract class constructors

---
+++ code/app/src/main/java/com/example/experiment_automata/Trial.java
@@ -0,0 +1,25 @@
+package com.example.experiment_automata;
+
+import android.location.Location;
+
+import java.util.Date;
+import java.util.UUID;
+
+public abstract class Trial {
+    private UUID collector;
+    private Location location;
+    private Date date;
+//    TODO: uncomment qr line once qr support has been added
+//    private QRcode qr;
+
+    public Trial(UUID collector) {
+        this.collector = collector;
+        this.date = new Date();
+    }
+
+    public Trial(UUID collector, Location location) {
+        this.collector = collector;
+        this.location = location;
+        this.date = new Date();
+    }
+}

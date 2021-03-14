eb6f23e716c28482cdb882f865f5a9381ad5d7e5
Pegmode <dchu@ualberta.ca>
2021-03-13 20:52:50 -0700

    added packQRString

--- code/app/src/main/java/com/example/experiment_automata/QRCode/QRCodeManager.java
+++ code/app/src/main/java/com/example/experiment_automata/QRCode/QRCodeManager.java
@@ -10,9 +10,9 @@ public class QRCodeManager {
 
 
     /**
-     * Unpacks a string from QR code into UUID
+     * Unpacks a string from custom ExperimentAutomata QRCode content to string
      * @param code
-     * packed QR string
+     * packed ExperimentAutomata QRCode string
      * @return
      * returns the content of the packed QR code String if it's valid, returns an empty string if it's not a valid Experiment Automata QR code
      */
@@ -26,6 +26,17 @@ public class QRCodeManager {
         else{
             return code.substring(4);
         }
+    }
+    /**
+     * Packs a string into custom ExperimentAutomata QR code
+     * @param content
+     * string to pack
+     * @return
+     * returns the custom ExperimentAutomata QR code
+     */
+    //keep as method in case we want to have special fields in the future
+    private String packQRString(String content){
+        return AUTOMATA_QR_HEADER + content;
     }
 
 }

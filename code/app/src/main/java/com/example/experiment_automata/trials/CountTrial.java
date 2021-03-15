5505f37653ef0015f2d4c3edd0679f0405f03abf
giancarlopernudisegura <pernudig@gmail.com>
2021-03-14 23:42:56 -0600

    temporary: generate graph functions for CountExperiment

--- code/app/src/main/java/com/example/experiment_automata/trials/CountTrial.java
+++ code/app/src/main/java/com/example/experiment_automata/trials/CountTrial.java
@@ -2,6 +2,11 @@ package com.example.experiment_automata.trials;
 
 import android.location.Location;
 
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class CountTrial extends Trial {
@@ -11,5 +16,43 @@ public class CountTrial extends Trial {
 
     public CountTrial(UUID collector, Location location) {
         super(collector, location);
+    }
+
+    /**
+     * Generate a list of entries needed to plot a histogram
+     * @param trials
+     *  trials to use for data
+     * @return
+     *  the list of entries that represent a histogram of trials.
+     */
+    public List<Entry> generateHistogram(Collection<Trial> trials) {
+        // Get data range counts into bins
+        final int amountOfBins = 1;
+        int[] bins = new int[amountOfBins];
+        for (int i = 0; i < amountOfBins; i++) { bins[i] = 0; }
+        for (Trial trial: trials) {
+            bins[0]++;
+        }
+        // Convert bins to entries
+        List<Entry> data = new ArrayList<>();
+        for (int i = 0; i < amountOfBins; i++) {
+            data.add(new Entry(i, bins[i]));
+        }
+        return data;
+    }
+
+    /**
+     * Generate a list of entries needed to plot results of trials.
+     * @param trials
+     *  trials to use for data
+     * @return
+     *  the list of entries that represent a plot
+     */
+    public List<Entry> generatePlot(Collection<Trial> trials) {
+        List<Entry> data = new ArrayList<>();
+        for (Trial trial : trials ) {
+            data.add(new Entry(trial.getDate().getTime(), 1));
+        }
+        return data;
     }
 }

dbc085ca2de41c310acf4a092a3297b62776d714
TurtleSquad007 <46603342+TurtleSquad007@users.noreply.github.com>
2021-03-18 10:27:13 -0600

    finished all question user stories intent

--- code/app/src/androidTest/java/com/example/experiment_automata/QuestionsUserStoriesTests.java
+++ code/app/src/androidTest/java/com/example/experiment_automata/QuestionsUserStoriesTests.java
@@ -19,6 +19,8 @@ import android.widget.ListView;
 import com.example.experiment_automata.Experiments.ExperimentModel.Experiment;
 import com.example.experiment_automata.Experiments.ExperimentModel.ExperimentMaker;
 import com.example.experiment_automata.Experiments.ExperimentModel.ExperimentType;
+import com.example.experiment_automata.QuestionsModel.Question;
+import com.example.experiment_automata.QuestionsModel.Reply;
 import com.example.experiment_automata.ui.home.HomeFragment;
 import com.robotium.solo.Solo;
 
@@ -30,6 +32,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -112,5 +115,120 @@ public class QuestionsUserStoriesTests {
         int afterAddingQuestionCount = currentTestingActivity.questionManager.getAllQuestions().size();
         assertEquals("Question not added make sure model works", false,
                 beforeAddingQuestionCount > afterAddingQuestionCount);
+    }
+
+    /**
+     * testing user story -- us.02.02.01
+     * Adding a reply to a question that the user has already entered
+     */
+    @Test
+    public void testingAddingReplies()
+    {
+        View replyButton = null;
+        View questionButton = null;
+
+        assertNotEquals("Can't find + button", null, addExperimentButton);
+
+        //Click from the home screen the + button to make an experiment
+        solo.clickOnView(addExperimentButton);
+        solo.waitForDialogToOpen();
+        descriptionEdit = solo.getView(R.id.create_experiment_description_editText);
+        assertNotEquals("Can't find description box", null, descriptionEdit);
+
+        //We need to fill the form to add the experiment
+        //Writing description
+        solo.clickOnView(descriptionEdit);
+        solo.enterText((EditText) descriptionEdit, "GUI Test Experiment");
+
+        //Writing min num trials
+        countTrialsEdit = solo.getView(R.id.experiment_min_trials_editText);
+        assertNotEquals("Can't find description box", null, countTrialsEdit);
+        solo.clickOnView(countTrialsEdit);
+        solo.enterText((EditText) countTrialsEdit, "3");
+
+        //Setting the boxes
+        location = solo.getView(R.id.experiment_require_location_switch);
+        acceptNewResults = solo.getView(R.id.experiment_accept_new_results_switch);
+        solo.clickOnView(location);
+        solo.clickOnView(acceptNewResults);
+        solo.clickOnText("Ok");
+
+        solo.clickOnText("GUI Test Experiment");
+        questionButton = solo.getView(R.id.nav_fragment_experiment_detail_view_qa_button);
+        solo.clickOnView(questionButton);
+        solo.clickOnView(addExperimentButton);
+        View questionBox = solo.getView(R.id.frag_add_edit_question_input_box_diolog);
+        solo.enterText((EditText)questionBox,"Test Question");
+        solo.clickOnText("Ok");
+
+        replyButton = solo.getView(R.id.main_question_display_reply_button);
+        solo.clickOnView(replyButton);
+        solo.enterText((EditText)questionBox,"Test Question");
+        solo.clickOnText("Ok");
+
+        Object[] x = (currentTestingActivity.questionManager.getAllQuestions()).toArray();
+        int size = ((ArrayList)(x[0])).size();
+
+        assertEquals("Failed tp add reply to question", 1, size);
+    }
+
+    /**
+     * Testing us.02.03.01
+     * Testing if the user can views the questions and replies that have been given.
+     * This test is working under the assumption that the user can only reply to the question
+     * once and each question can have more replies.
+     *
+     * For part 4: This will change to allow the nesting of questions such that a question can
+     * have more than one reply.
+     */
+    @Test
+    public void testingUserCanSeeQuestionAndReplies()
+    {
+        View replyButton = null;
+        View questionButton = null;
+
+        assertNotEquals("Can't find + button", null, addExperimentButton);
+
+        //Click from the home screen the + button to make an experiment
+        solo.clickOnView(addExperimentButton);
+        solo.waitForDialogToOpen();
+        descriptionEdit = solo.getView(R.id.create_experiment_description_editText);
+        assertNotEquals("Can't find description box", null, descriptionEdit);
+
+        //We need to fill the form to add the experiment
+        //Writing description
+        solo.clickOnView(descriptionEdit);
+        solo.enterText((EditText) descriptionEdit, "GUI Test Experiment");
+
+        //Writing min num trials
+        countTrialsEdit = solo.getView(R.id.experiment_min_trials_editText);
+        assertNotEquals("Can't find description box", null, countTrialsEdit);
+        solo.clickOnView(countTrialsEdit);
+        solo.enterText((EditText) countTrialsEdit, "3");
+
+        //Setting the boxes
+        location = solo.getView(R.id.experiment_require_location_switch);
+        acceptNewResults = solo.getView(R.id.experiment_accept_new_results_switch);
+        solo.clickOnView(location);
+        solo.clickOnView(acceptNewResults);
+        solo.clickOnText("Ok");
+
+        solo.clickOnText("GUI Test Experiment");
+        questionButton = solo.getView(R.id.nav_fragment_experiment_detail_view_qa_button);
+        solo.clickOnView(questionButton);
+        solo.clickOnView(addExperimentButton);
+        View questionBox = solo.getView(R.id.frag_add_edit_question_input_box_diolog);
+        solo.enterText((EditText)questionBox,"Test Question");
+        solo.clickOnText("Ok");
+
+        replyButton = solo.getView(R.id.main_question_display_reply_button);
+        solo.clickOnView(replyButton);
+        solo.enterText((EditText)questionBox,"Test reply");
+        solo.clickOnText("Ok");
+
+        boolean replyQuestionOnScreen = solo.searchText("Test Question") && solo.searchText("Test reply");
+        assertEquals("Failed to display the questions and reply onto the screen",
+                true,
+                replyQuestionOnScreen);
     }
 }

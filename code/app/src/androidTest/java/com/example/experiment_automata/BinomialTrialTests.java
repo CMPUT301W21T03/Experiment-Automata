49fb57a1e73c5c99419e7aa29cde91eec3b595ba
TurtleSquad007 <46603342+TurtleSquad007@users.noreply.github.com>
2021-03-19 13:27:28 -0600

    Adding intent test results and trials

--- code/app/src/androidTest/java/com/example/experiment_automata/BinomialTrialTests.java
+++ code/app/src/androidTest/java/com/example/experiment_automata/BinomialTrialTests.java
@@ -1,9 +1,20 @@
 package com.example.experiment_automata;
 
 
+/**
+ * Some experiment user story tests done here
+ *
+ * US 01.06.01
+ * US 01.07.01
+ * US 01.08.01
+ * US 01.09.01
+ *
+ */
+
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.experiment_automata.Experiments.ExperimentModel.Experiment;
 import com.example.experiment_automata.Experiments.ExperimentModel.ExperimentMaker;
@@ -100,25 +111,89 @@ public class BinomialTrialTests {
     private void addBinomalTrial(boolean click)
     {
         solo.clickOnView(addExperimentButton);
-        View binmoailTrailRadio = solo.getView(R.id.add_binomial_value);
-        if(click)
-            solo.clickOnView(binmoailTrailRadio);
+        if(click) {
+            solo.waitForText("Passed");
+            solo.clickOnText("Passed");
+        }
+        addExperimentButton = currentTestingActivity.findViewById(R.id.add_experiment_button);
+        solo.clickOnView(addExperimentButton);
     }
 
 
+    /**
+     * Testing US 01.08.01 here we're ignoring a trial and making sure the
+     * quartile sentence changes to meet that when the trial is ignored
+     */
     @Test
     public void testIntentIgnoringTrials() {
         String testDes = "Testing Intent";
         makeExperiment(testDes);
         solo.clickOnText(testDes);
-        solo.clickOnView(addExperimentButton);
-        // Adding 5 trial
-        for (int i = 0; i < 5; i++) {
+        // Adding 3 trial
+        for (int i = 0; i < 3; i++) {
             addBinomalTrial(i % 2 == 0);
         }
+        View qur = solo.getView(R.id.quartiles_text);
+        String qur1 = ((TextView)qur).getText().toString();
+        solo.clickOnView(solo.getView(R.id.trial_ignore_checkbox));
+        View qur2 = solo.getView(R.id.mean_value);
+        String qur3 = ((TextView)qur2).getText().toString();
 
+        assertNotEquals("Mean is the same when it should not be", qur1, qur3);
+    }
 
+    /**
+     * Testing US 01.09.01 to see if IQR values are displayed correctly.
+     * After finish adding some number of trials
+     */
+    @Test
+    public void testIntentIQRNumbersAppear()
+    {
+        Double medianTestValue = 1.000;
+        Double meanTestValue = 1.000;
+        String testDes = "Testing Intent";
+        makeExperiment(testDes);
+        solo.clickOnText(testDes);
+        // Adding 4 trial
+        for (int i = 0; i < 4; i++) {
+            addBinomalTrial(true);
+        }
+        String quertile = ((TextView)solo.getView(R.id.quartiles_value)).getText().toString();
+        Double median = new Double(((TextView)solo.getView(R.id.median_value)).getText().toString());
+        Double mean = new Double(((TextView)solo.getView(R.id.mean_value)).getText().toString());
+        Double std = new Double(((TextView)solo.getView(R.id.stdev_text)).getText().toString());
 
-        assertNotEquals("Mean is the same when it should not be", 1, 1);
+        assertNotEquals("Standard div wrong", std, null);
+        assertEquals("Mean wrong", meanTestValue, mean, 0.00001);
+        assertEquals("Median wrong", medianTestValue, median, 0.00001);
+        assertNotEquals("Quartile set wrong", null, quertile);
+    }
+
+    /**
+     * Testing US 01.06.01 check to see if the title shows as we cannot
+     * really compare graphs without overcomplicated results
+     */
+    @Test
+    public void testIntentChartHistogramDataDisplayed()
+    {
+        String testDes = "Testing Intent";
+        makeExperiment(testDes);
+        solo.clickOnText(testDes);
+        addBinomalTrial(true);
+        assertNotEquals("Chart data not displayed", true, solo.searchText("Histogram"));
+    }
+
+    /**
+     * Testing US 01.07.01 check to see if the title shows as we cannot
+     * really compare graphs without overcomplicated results
+     */
+    @Test
+    public void testIntentChartPlotDataDisplayed()
+    {
+        String testDes = "Testing Intent";
+        makeExperiment(testDes);
+        solo.clickOnText(testDes);
+        addBinomalTrial(true);
+        assertNotEquals("Chart data not displayed", true, solo.searchText("Results"));
     }
 }

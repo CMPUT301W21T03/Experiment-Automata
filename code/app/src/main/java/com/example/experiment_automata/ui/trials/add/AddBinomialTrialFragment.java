db15df27ccc7b359c263fcbf973ed480769a2514
Pegmode <dchu@ualberta.ca>
2021-03-25 11:48:23 -0600

    Added basic scanner to binomial trials

--- code/app/src/main/java/com/example/experiment_automata/ui/trials/add/AddBinomialTrialFragment.java
+++ code/app/src/main/java/com/example/experiment_automata/ui/trials/add/AddBinomialTrialFragment.java
@@ -1,17 +1,19 @@
 package com.example.experiment_automata.ui.trials.add;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.experiment_automata.R;
 import com.example.experiment_automata.backend.experiments.BinomialExperiment;
 import com.example.experiment_automata.ui.NavigationActivity;
-import com.example.experiment_automata.R;
+import com.example.experiment_automata.ui.qr.ScannerActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,14 +21,13 @@ import com.example.experiment_automata.R;
  * create an instance of this fragment.
  */
 public class AddBinomialTrialFragment extends Fragment {
+    private ImageButton scanQRButton;
     public AddBinomialTrialFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,6 +38,16 @@ public class AddBinomialTrialFragment extends Fragment {
         NavigationActivity parentActivity = ((NavigationActivity) getActivity());
         BinomialExperiment experiment = (BinomialExperiment) parentActivity.experimentManager.getCurrentExperiment();
         description.setText(experiment.getDescription());
+
+        scanQRButton = root.findViewById(R.id.binomial_trial_experiment_description_qr_button);
+        scanQRButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ScannerActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 }

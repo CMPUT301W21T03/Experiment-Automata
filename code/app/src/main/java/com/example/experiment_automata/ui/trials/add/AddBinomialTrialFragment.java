37ce746e4b487980f63dbb1e97fda7182a9f241b
TurtleSquad007 <46603342+TurtleSquad007@users.noreply.github.com>
2021-03-27 18:58:00 -0600

    small typo fix

--- code/app/src/main/java/com/example/experiment_automata/ui/trials/add/AddBinomialTrialFragment.java
+++ code/app/src/main/java/com/example/experiment_automata/ui/trials/add/AddBinomialTrialFragment.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.example.experiment_automata.backend.experiments.BinomialExperiment;

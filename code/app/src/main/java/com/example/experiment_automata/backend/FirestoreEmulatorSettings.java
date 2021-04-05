361caa3585385cde8eb8b345a5e0e42ffe823dff
Pegmode <dchu@ualberta.ca>
2021-04-05 16:28:52 -0600

    added FirestoreEmulatorSettings

---
+++ code/app/src/main/java/com/example/experiment_automata/backend/FirestoreEmulatorSettings.java
@@ -0,0 +1,36 @@
+package com.example.experiment_automata.backend;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+
+/**
+ * Role/Pattern:
+ *     Sets up firestore for emulator
+ *
+ *  Known Issue:
+ *
+ *      1. None
+ */
+public class FirestoreEmulatorSettings {
+    static public final boolean USE_EMULATOR = true;// This value controls
+    /**
+     * Setup firestore to use Firebase CLI firestore emulator if enabled
+     * @return
+     *  If the emulator is in use
+     */
+    static public boolean setupFirestoreEmulator(){
+        if (USE_EMULATOR) {
+            // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+            // the host computer from an Android emulator.
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            firestore.useEmulator("10.0.2.2", 8080);
+
+
+            FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                    .setPersistenceEnabled(false)
+                    .build();
+            firestore.setFirestoreSettings(settings);
+        }
+        return USE_EMULATOR;
+    }
+}

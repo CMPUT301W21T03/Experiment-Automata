7d8d283e5f3ca8b646b627a0156d520921589222
TurtleSquad007 <46603342+TurtleSquad007@users.noreply.github.com>
2021-04-07 15:19:48 -0600

    started work on user manager tests

---
+++ code/app/src/test/java/com/example/experiment_automata/UserManagerModelTests.java
@@ -0,0 +1,11 @@
+package com.example.experiment_automata;
+
+import com.example.experiment_automata.backend.users.UserManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserManagerModelTests
+{
+    private UserManager userManager;
+}

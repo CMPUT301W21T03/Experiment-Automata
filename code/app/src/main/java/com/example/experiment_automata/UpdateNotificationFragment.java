e6ed9506ffd155e5274ad77e4dfc097af3655d23
giancarlopernudisegura <pernudig@gmail.com>
2021-04-07 11:32:25 -0600

    refresh fragment

---
+++ code/app/src/main/java/com/example/experiment_automata/UpdateNotificationFragment.java
@@ -0,0 +1,27 @@
+package com.example.experiment_automata;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class UpdateNotificationFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_notification, container, false);
+    }
+}

0d236677805fac5bfce2a39f10a003af9fd3dd47
Jordan <jdvandenbruel@gmail.com>
2021-04-08 12:06:00 -0600

    add question tests up to getExperiment

--- code/app/src/test/java/com/example/experiment_automata/QuestionTest.java
+++ code/app/src/test/java/com/example/experiment_automata/QuestionTest.java
@@ -31,4 +31,47 @@ public class QuestionTest {
         assertEquals(experiment, question.getExperimentId());
         assertEquals(questionID, question.getQuestionId());
     }
+
+    @Test
+    public void testGetQuestion() {
+        Question question = new Question(description, owner, experiment, questionID);
+        assertEquals(description, question.getQuestion());
+        assertNotEquals("random string", question.getQuestion());
+    }
+
+    @Test
+    public void testSetQuestion() {
+        Question question = new Question(description, owner, experiment, questionID);
+        String newQuestion = "New Question";
+        assertEquals(description, question.getQuestion());
+        assertNotEquals(newQuestion, question.getQuestion());
+        question.setQuestion(newQuestion);
+        assertNotEquals(description, question.getQuestion());
+        assertEquals(newQuestion, question.getQuestion());
+    }
+
+    @Test
+    public void testGetUser() {
+        Question question = new Question(description, owner, experiment, questionID);
+        assertEquals(owner, question.getUser());
+        assertNotEquals(UUID.randomUUID(), question.getUser());
+    }
+
+    @Test
+    public void testSetUser() {
+        Question question = new Question(description, owner, experiment, questionID);
+        UUID newOwner = UUID.randomUUID();
+        assertEquals(owner, question.getUser());
+        assertNotEquals(newOwner, question.getUser());
+        question.setUser(newOwner);
+        assertEquals(newOwner, question.getUser());
+        assertNotEquals(owner, question.getUser());
+    }
+
+    @Test
+    public void testGetExperiment() {
+        Question question = new Question(description, owner, experiment, questionID);
+        assertEquals(experiment, question.getExperimentId());
+        assertNotEquals(UUID.randomUUID(), question.getExperimentId());
+    }
 }

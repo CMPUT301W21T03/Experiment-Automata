b2ae341205671d1583a6e36d5b712ece1ad30817
giancarlopernudisegura <pernudig@gmail.com>
2021-04-08 22:29:31 -0600

    add count stat unit test

--- code/app/src/test/java/com/example/experiment_automata/UserManagerModelTest.java
+++ code/app/src/test/java/com/example/experiment_automata/UserManagerModelTest.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class UserManagerModelTests {
+public class UserManagerModelTest {
     /**
      * Role: Testing the UserManager class
      */
